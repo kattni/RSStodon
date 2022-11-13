5121bde729028ba4b9fefb46d3aeed2f53da82a7
Kattni Rembor <kattni@adafruit.com>
2022-11-12 19:14:56 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,63 @@
+# TwitteRSS
+## Parse an RSS Feed and Post Content to Twitter and Mastodon
+
+### Code usage
+**If you have not already, please complete the prerequisites below before running the script.**
+
+To use this example, from the directory containing the script file, simply run the following from command line:
+
+```commandline
+python tweet_toot.py
+```
+Note: If Python 2 is your default version, you may need to modify the command to begin with `python3`.
+
+### Prerequisites
+1. You must have Python 3 installed on your computer, along with the `pip` installer. If you have not already done so, search for the instructions specific to your computer hardware and operating system.
+
+2. Use `pip` to install the packages within the _requirements.txt_ file. From within the same directory as the _requirements.txt_ file, run the following:
+    ```commandline
+    pip install -r requirements.txt
+    ```
+   Note: If Python 3 is not your default version, you may have to modify the command to begin with `pip3`.
+3. You must acquire API access for both Twitter and Mastodon. Please check the documentation for each platform for information on how to obtain the appropriate keys.
+   * For Twitter, this means you should obtain the following:
+     * Twitter API key
+     * Twitter API key secret
+     * Twitter access token
+     * Twitter access token secret
+     * Twitter bearer token
+
+   * For Mastodon, this means you should obtain the following:
+     * Mastodon access token
+
+4. Once all six keys are obtained, they should be added to a _keys.py_ file in the same directory as this script, as strings assigned to the following list of variables.
+      * TWITTER_API_KEY
+      * TWITTER_API_KEY_SECRET
+      * TWITTER_ACCESS_TOKEN
+      * TWITTER_ACCESS_TOKEN_SECRET
+      * TWITTER_BEARER_TOKEN
+      * MASTODON_ACCESS_TOKEN
+
+   For example, the first line in keys.py should look similar to the following:
+   ```python
+   TWITTER_API_KEY = "yourtwitterapikeyhere"
+   ```
+
+5. Create an empty file named _string_cache.txt_ in the same directory from which you plan to run this script. The code checks for it and fails with instructions if you have not already done this before running the script.
+
+6. In the `** CUSTOMISATIONS **` section at the beginning of the code, update each of the variables to your desired settings or content. The customisable options are as follows:
+    * Disable one or both social media platforms from sending, by updating `True` on one or both of the following lines to `False`. Both default to `True` meaning both are enabled by default.
+      ```python
+      SEND_TO_TWITTER = True
+      SEND_TO_MASTODON = True
+      ```
+    * Update mastodon_server_url to your Mastodon server URL as a string. Defaults to `"https://octodon.social"`.
+      ```python
+      mastodon_server_url = "https://octodon.social"
+      ```
+    * Update the feed URL to match the URL of the RSS feed you intend to parse. Defaults to the kattni.com blog RSS feed.
+      ```python
+      feed = feedparser.parse("https://kattni.com/feeds/all.atom.xml")
+      ```
+
+Once you've completed the steps above, you're ready to run the script!
---
+++ Twitter-and-Mastodon/tweet_toot.py
@@ -0,0 +1,153 @@
+# SPDX-FileCopyrightText: 2022 Kattni Rembor
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""
+Parse an RSS feed and generate a string based on the content, and send that string as a message to
+Twitter and Mastodon.
+
+PREREQUISITES FOR RUNNING THIS CODE
+Note: Prerequisites are also detailed in the README on GitHub.
+1. You MUST acquire API access for both Twitter and Mastodon. Please check the documentation for
+each platform for information on how to obtain the appropriate keys.
+    For Twitter, this means you should obtain the following:
+    * Twitter API key
+    * Twitter API key secret
+    * Twitter access token
+    * Twitter access token secret
+    * Twitter bearer token
+    For Mastodon, this means you should obtain the following:
+    * Mastodon access token
+
+2. Once all six keys are obtained, they should be added to a keys.py file in the same directory as
+this script, as strings assigned to the following list of variables.
+    * TWITTER_API_KEY
+    * TWITTER_API_KEY_SECRET
+    * TWITTER_ACCESS_TOKEN
+    * TWITTER_ACCESS_TOKEN_SECRET
+    * TWITTER_BEARER_TOKEN
+    * MASTODON_ACCESS_TOKEN
+    For example, the first line in keys.py should look similar to the following:
+    TWITTER_API_KEY = "yourtwitterapikeyhere"
+
+3. Create an empty file named string_cache.txt in the same directory from which you plan to run
+this script. The code checks for it and fails with instructions if you have not already done this
+before running the script.
+
+4. In the CUSTOMISATIONS section at the beginning of the code, update each of the variables to
+your desired settings or content. Instructions for each included in the code comments.
+"""
+
+from pathlib import Path
+import feedparser
+import tweepy
+import mastodon
+from keys import (
+    TWITTER_API_KEY,
+    TWITTER_API_KEY_SECRET,
+    TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_TOKEN_SECRET,
+    TWITTER_BEARER_TOKEN,
+    MASTODON_ACCESS_TOKEN,
+)
+
+# ** CUSTOMISATIONS **
+# Disable one or both of the platforms by setting the related variable to False. Defaults to True.
+SEND_TO_TWITTER = True
+SEND_TO_MASTODON = True
+
+# Update to match your Mastodon server base URL. Defaults to "https://octodon.social".
+mastodon_server_url = "https://octodon.social"
+
+# Update this URL to match the RSS feed of your choice. Defaults to kattni.com feed.
+feed = feedparser.parse("https://kattni.com/feeds/all.atom.xml")
+
+
+# ** CODE **
+# Set up for duplicate post checking.
+# Creates a variable to use as this instance is included multiple times throughout this script.
+string_cache = Path("string_cache.txt")
+
+# If the file string_cache.txt does not exist...
+if not string_cache.exists():
+    # ...fail here with instructions to create the file.
+    raise Exception(f"{string_cache} does not exist. "
+                    f"Create an empty file named {string_cache} in "
+                    f"the same directory as this script.")
+
+# Hashtag generation.
+# Creates an empty string to which to append tags.
+tags_str = ""
+# Try to parse "tags" out of the RSS feed.
+try:
+    # Loop through tags to obtain the tags.
+    for tag_obj in feed["items"][0]["tags"]:
+        # Filter out tags that are numeric (i.e. years). This was added as a workaround due to
+        # an issue that causes inclusion of the current year as a tag even though it is not
+        # explicitly included in the tag list.
+        if not tag_obj['term'].isnumeric():
+            # Append each tag to string with space and hashtag.
+            tags_str = f"{tags_str} #{tag_obj['term']}"
+    # Trim the leading space.
+    tags_str = tags_str[1:]
+# If there is no "tags" entry in the RSS feed, continue on to the next section.
+except KeyError:
+    pass
+
+# Message content generation.
+# Generate the message content from the parsed RSS feed.
+message_template_string = (
+    f"{feed['items'][0]['title']} {feed['items'][0]['links'][0]['href']} {tags_str}"
+)
+
+# Duplicate post check.
+# Avoids sending duplicate messages by verifying that the message template string
+# generated above is not identical to the last string sent using this script.
+if string_cache.read_text() == message_template_string:
+    raise Exception("Message template string has not been updated.")
+
+# API Authentication.
+# try/except used here because the default error is not clear as to why it failed.
+try:
+    # Authenticate to Twitter.
+    client = tweepy.Client(
+        TWITTER_BEARER_TOKEN,
+        TWITTER_API_KEY,
+        TWITTER_API_KEY_SECRET,
+        TWITTER_ACCESS_TOKEN,
+        TWITTER_ACCESS_TOKEN_SECRET,
+    )
+    # Attempts to access Twitter API to verify authentication.
+    client.get_me()
+# If Twitter authentication fails, except confusing error...
+except tweepy.errors.Unauthorized:
+    # ...and raise clearer one.
+    raise Exception("Authentication failed. Check your keys.")
+
+# try/except used here for consistency. The Mastodon authentication failure is clearer.
+try:
+    # Authenticate to Mastodon.
+    mastodon = mastodon.Mastodon(
+        access_token=MASTODON_ACCESS_TOKEN, api_base_url=mastodon_server_url
+    )
+    # Attempts to access Mastodon API to verify authentication.
+    mastodon.me()
+# If authentication fails, except Mastodon default error...
+except mastodon.Mastodon.MastodonUnauthorizedError:
+    # ...and raise the same as above.
+    raise Exception("Authentication failed. Check your keys.")
+
+# Send messages.
+# If Twitter is enabled (True) above...
+if SEND_TO_TWITTER:
+    # Send the generated string as a message to Twitter.
+    client.create_tweet(text=message_template_string)
+
+# If Mastodon is enabled (True) above...
+if SEND_TO_MASTODON:
+    # Send the generated string as a message to Mastodon.
+    mastodon.toot(message_template_string)
+
+# Update string cache.
+# Once both messages are sent successfully, update the content of string_cache.txt to the latest
+# message template string for comparison the next time this script is run.
+string_cache.write_text(message_template_string)
